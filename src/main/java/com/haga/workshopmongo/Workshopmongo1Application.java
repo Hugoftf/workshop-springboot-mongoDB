2f68331dbf041fc5f06198450b9dc54f809e368c
Hugo de França <f3rreirahugo@hotmail.com>
2023-11-15 20:06:54 -0300

    First commit, project created

---
+++ src/main/java/com/haga/workshopmongo/Workshopmongo1Application.java
@@ -0,0 +1,13 @@
+package com.haga.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Workshopmongo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Workshopmongo1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/haga/workshopmongo/Workshopmongo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haga.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Workshopmongo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
