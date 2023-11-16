9a59b99987555afe10f8dd8d996ed0b0519a1563
Hugo de França <f3rreirahugo@hotmail.com>
2023-11-16 16:19:30 -0300

    data projection

--- src/main/java/com/haga/workshopmongo/config/Instantiation.java
+++ src/main/java/com/haga/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.haga.workshopmongo.domain.Post;
 import com.haga.workshopmongo.domain.User;
+import com.haga.workshopmongo.dto.AuthorDTO;
 import com.haga.workshopmongo.repository.PostRepository;
 import com.haga.workshopmongo.repository.UserRepository;
 
@@ -36,10 +37,12 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"),"Partiu viajar", "Vou viajar para São Paulo", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"),"Bom dia",  "Acordei feliz hoje", maria);
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"),"Partiu viajar", "Vou viajar para São Paulo", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"),"Bom dia",  "Acordei feliz hoje", new AuthorDTO(maria));
+		
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
