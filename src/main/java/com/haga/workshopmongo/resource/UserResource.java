c894c30cc67e806fb31354b59be8b3081eb72dcd
Hugo de França <f3rreirahugo@hotmail.com>
2023-11-16 13:27:11 -0300

    insert users with POST

--- src/main/java/com/haga/workshopmongo/resource/UserResource.java
+++ src/main/java/com/haga/workshopmongo/resource/UserResource.java
@@ -1,5 +1,6 @@
 package com.haga.workshopmongo.resource;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,8 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.haga.workshopmongo.domain.User;
 import com.haga.workshopmongo.dto.UserDTO;
@@ -32,6 +36,14 @@ public class UserResource {
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		User obj = userService.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
+	
+	@PostMapping	
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDTO) {
+		User obj = userService.fromDTO(objDTO);
+		obj = userService.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 	
 }
--- src/main/java/com/haga/workshopmongo/service/UserService.java
+++ src/main/java/com/haga/workshopmongo/service/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.haga.workshopmongo.domain.User;
+import com.haga.workshopmongo.dto.UserDTO;
 import com.haga.workshopmongo.repository.UserRepository;
 import com.haga.workshopmongo.service.exception.ObjectNotFoundException;
 
@@ -25,4 +26,14 @@ public class UserService {
 		Optional<User> user = userRepository.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
+	
+	public User insert(User obj) {
+		return userRepository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
+	
+	
 }
