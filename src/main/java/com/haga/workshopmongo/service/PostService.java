d36e5994fa7f1ee45ee652e1238c2a7948db08c6
Hugo de França <f3rreirahugo@hotmail.com>
2023-11-16 19:19:20 -0300

    Query with multiple criteria

--- src/main/java/com/haga/workshopmongo/service/PostService.java
+++ src/main/java/com/haga/workshopmongo/service/PostService.java
@@ -1,5 +1,6 @@
 package com.haga.workshopmongo.service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,4 +27,11 @@ public class PostService {
 	public List<Post> findByTitle(String text){
 		return postRepository.findByTitle(text);
 	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return postRepository.fullsearch(text, minDate, maxDate);
+	}
+	
+	
 }
