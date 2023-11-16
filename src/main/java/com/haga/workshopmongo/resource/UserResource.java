b85a1cef4978274bae27d3235a0323ae7b38520f
Hugo de França <f3rreirahugo@hotmail.com>
2023-11-16 15:15:19 -0300

    update users with PUT

--- src/main/java/com/haga/workshopmongo/resource/UserResource.java
+++ src/main/java/com/haga/workshopmongo/resource/UserResource.java
@@ -53,4 +53,12 @@ public class UserResource {
 		return ResponseEntity.noContent().build();
 	}
 	
+	@PostMapping(value="/{id}")
+	public ResponseEntity<Void> update(@RequestBody UserDTO objDTO, @PathVariable String id) {
+		User obj = userService.fromDTO(objDTO);
+		obj.setId(id);
+		obj = userService.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+	
 }
